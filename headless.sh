277e56d01162ac1a892b85b510c7baf4d7067c76
Meghthedev <meghthedev@gmail.com>
2023-12-31 08:21:49 +0530

    Releases: add script to upload zip files headlessly
    * its a modified version of multi_upload.sh, to upload all zip files ( more than 1) to github releases without any user input. to be called by another script

---
+++ headless.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set Github Release version name from names of file
+version=$(ls *.zip | tr '\n' ' ')
+
+# Check if the tag already exists
+if git rev-parse "$version" &> /dev/null; then
+  echo "Error: The release tag '$version' already exists."
+  exit 1
+fi
+
+# Create the tag and push it to GitHub
+git tag -a "$version" -m "Release $version"
+git push origin "$version"
+
+# Upload all .zip files in the current directory
+filenames=(*.zip)
+
+# Create the release on GitHub
+if ! gh release create "$version" --title "Release $version" --notes "Release notes"; then
+  echo "Error: Failed to create the release."
+  exit 1
+fi
+
+# Upload the files to the release
+for filename in "${filenames[@]}"; do
+  gh release upload "$version" "$filename" --clobber
+done
+
+# Display success message
+echo "Files uploaded successfully."
